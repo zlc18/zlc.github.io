e60879c48a3d88593df900b82487337c4bc5334b
zlc18 <2827212797@qq.com>
2019-08-18 09:44:49 +0800

    Site updated: 2019-08-18 09:44:48

--- dist/music.js
+++ dist/music.js
@@ -1,24 +1,15 @@
 const ap = new APlayer({
-    container: document.getElementById('player'),
-    listFolded: false,//列表默认折叠
-    listMaxHeight: 90,//列表最大高度
-    lrcType: 3, //此为歌词格式，没有歌词可以直接删掉这一行
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    lrcType: 3,
     audio: [
-        {
-            name: 'name1',
-            artist: 'artist1',
-            url: 'url1.mp3',
-            cover: 'cover1.jpg',
-            lrc: 'lrc1.lrc',
-            theme: '#ebd0c2'
-        },
-        {
-            name: 'name2',
-            artist: 'artist2',
-            url: 'url2.mp3',
-            cover: 'cover2.jpg',
-            lrc: 'lrc2.lrc',
-            theme: '#46718b'
-        }
+      {
+        name: "在水一方",
+        artist: '李健',
+        url: 'http://other.web.ri01.sycdn.kuwo.cn/resource/n2/31/23/2648161877.mp3',
+        cover: 'http://star.kuwo.cn/star/starheads/180/73/49/1131928591.jpg',
+        lrc: 'song-post/zaishuiyifang.lrc'
+      },
     ]
 });
