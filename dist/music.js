a47f0d755621d0c770c37e871d884149233ec5cc
zlc18 <2827212797@qq.com>
2019-08-18 13:39:18 +0800

    Site updated: 2019-08-18 13:39:16

--- dist/music.js
+++ dist/music.js
@@ -1,15 +1,15 @@
 const ap = new APlayer({
     container: document.getElementById('player'),
-    listFolded: false,//列表默认折叠
+    listFolded: true,//列表默认折叠
     listMaxHeight: 90,//列表最大高度
-    lrcType: 3, //此为歌词格式，没有歌词可以直接删掉这一行
+//  lrcType: 3, //此为歌词格式，没有歌词可以直接删掉这一行
     audio: [
         {
             name: '陪你度过漫长岁月',
             artist: '陈奕迅',
             url: 'http://www.ytmp3.cn/down/33576.mp3',
             cover: '/img/suiyueshentou.png',
-            lrc: 'lrc1.lrc',
+//          lrc: 'lrc1.lrc',
             theme: '#ebd0c2'
         },
         {
@@ -17,7 +17,7 @@ const ap = new APlayer({
             artist: '金岐玟',
             url: 'http://www.ytmp3.cn/down/44537.mp3',
             cover: '/img/suiyueshentou.png',
-            lrc: 'lrc2.lrc',
+//          lrc: 'lrc2.lrc',
             theme: '#46718b'
         }
     ]
