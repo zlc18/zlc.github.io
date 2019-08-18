3fbcdba988c08e16b8ce6fb406e84a054b4cedcd
zlc18 <2827212797@qq.com>
2019-08-18 14:13:10 +0800

    Site updated: 2019-08-18 14:13:09

--- dist/music.js
+++ dist/music.js
@@ -3,18 +3,22 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-      {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
-      },
-      {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
-      },
+       {
+            name: '陪你度过漫长岁月',
+            artist: '陈奕迅',
+            url: 'http://www.ytmp3.cn/down/33576.mp3',
+            cover: '/img/suiyueshentou.png',
+           
+           
+        },
+        
+        {
+            name: '岁月神偷',
+            artist: '金岐玟',
+            url: 'http://www.ytmp3.cn/down/44537.mp3',
+            cover: '/img/suiyueshentou.png',
+        },
+        
       {
         name: '风筝误',
         artist: '刘珂矣',
